e83eca11ac67fbd0e36e1027fa34796f7fdda3bd
Vladimir Sapozhnikov <Sapozhnikovl@yandex.ru>
2025-03-08 11:24:18 +0800

    Исправляет проект в соответствии с замечаниями, Добавляет проверку ввода имени, добавляет авторство в package.json

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,14 @@ import readLineSync from 'readline-sync';
 
 const greetUser = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readLineSync.question('May I have your name?');
+  let userName;
+  do {
+    userName = readLineSync.question('May I have your name?');
+    if (userName === null || userName.trim() === '') {
+      console.log(`The name cannot be empty. Please enter your name.`);
+    }
+  } while (userName === null || userName.trim() === '');
+
   console.log(`Hello, ${userName}!`);
   return userName;
 };
